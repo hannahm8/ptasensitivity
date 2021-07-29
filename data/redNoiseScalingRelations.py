a599b46bf35db57256afaf6f5e907230caa6fd58
hannahm <hmiddleton8@googlemail.com>
2021-07-29 16:45:24 +1000

    edits

--- data/redNoiseScalingRelations.py
+++ data/redNoiseScalingRelations.py
@@ -16,7 +16,7 @@ def gammaToGammaSN():
 
 
 
-def sigmaRNToAr(sigmaSN): 
+def sigmaSNToAr(sigmaSN): 
     """ 
     from equation 15 in M. Lan+2017 
     """
@@ -31,11 +31,11 @@ def sigmaRNToAr(sigmaSN):
     A = A * 1E-6 # in s yr^(1/2)
     A = A /(365.25*60*60*24) #in yr^{3/2}
     
-    return np.log10(A)
-    
+    return np.log10(A), gammaSN
+    #return A, gammaSN
 
 
-def redNoiseScalingRelation(f,fDotm15,Tyr):
+def convertF0F1ToSigmaSN(f,fDotm15,Tyr):
 
     """
     functoin takes
@@ -47,19 +47,27 @@ def redNoiseScalingRelation(f,fDotm15,Tyr):
     modFdot = abs(fDotm15)
 
     # vales from NANO+MSP10PPTA fit in Lam_17 ua(table 4)
-    C2    = 10.**(-1.2)
-    alpha = -0.9
-    beta  = 0.8
-    gamma = 2.3
+    #C2    = np.exp(-1.2)
+    #alpha = -0.9
+    #beta  = 0.8
+    #gamma = 2.3
+    C2 = np.exp(-16.)
+    alpha = 0.8
+    beta = 0.9
+    gamma = 4.9
 
     sigmaSN = C2 * (f**alpha) * (modFdot**beta) * (Tyr**gamma)
 
     return sigmaSN *1E-6
 
 
-mu = 245.4261196602377
-mudot = -5.38155E-16 / 1E-15
+
+
+"""
+mu = 339.3156872882446
+mudot = -1.614817E-15/ 1E-15
 Tyr = 10.
 
 sigmaSN = redNoiseScalingRelation(mu,mudot,Tyr)
 print(sigmaRNToAr(sigmaSN))
+"""
